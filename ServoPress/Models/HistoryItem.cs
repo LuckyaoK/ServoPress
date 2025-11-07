feb839f254f8490801da571a51bdd940cf09df5a
YaoKai <53244697@qq.com>
2025-11-07 16:23:33 +0800

    添加项目文件。

---
+++ ServoPress/Models/HistoryItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoPress.Models
+{
+    public class HistoryItem
+    {
+        public string Timestamp { get; set; }
+        public string Program { get; set; }
+        public string Result { get; set; }
+        public double MaxForce { get; set; }
+    }
+}
---
+++ ServoPress/Models/SequenceStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServoPress.Models
+{
+    public class SequenceStep
+    {
+        public int Step { get; set; }
+        public string Command { get; set; }
+        public string Params { get; set; }
+    }
+
+}
