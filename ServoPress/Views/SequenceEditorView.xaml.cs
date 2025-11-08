8bfc6a989491a00bc2c25fa92110ef66e2a38510
YaoKai <53244697@qq.com>
2025-11-08 08:40:47 +0800

    序列编辑器修改

--- ServoPress/Views/SequenceEditorView.xaml.cs
+++ ServoPress/Views/SequenceEditorView.xaml.cs
@@ -23,6 +23,53 @@ namespace ServoPress.Views
         public SequenceEditorView()
         {
             InitializeComponent();
+            // 默认显示第一个编辑器 (动作)
+            if (DetailEditorTabs != null)
+            {
+                DetailEditorTabs.SelectedIndex = 0;
+            }
+        }
+
+        // --- 事件处理器: 切换第三列的 TabControl ---
+
+        private void AddMotionButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 0;
+        }
+
+        private void AddMeasureButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 1;
+        }
+
+        private void AddTimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 2;
+        }
+
+        private void AddWaitButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 3;
+        }
+
+        private void AddLabelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 4;
+        }
+
+        private void AddJumpButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 5;
+        }
+
+        private void AddHomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 6;
+        }
+
+        private void AddEndButton_Click(object sender, RoutedEventArgs e)
+        {
+            DetailEditorTabs.SelectedIndex = 7;
         }
     }
 }
