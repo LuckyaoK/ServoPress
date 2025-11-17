a62605a373afeee9e875aca0fc902f2483a3262f
YaoKai <53244697@qq.com>
2025-11-17 14:59:15 +0800

    添加行为

---
+++ ServoPress/Behaviors/WindowBehavior.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace ServoPress.Behaviors
+{
+    /// <summary>
+    /// 包含用于窗口的附加行为 (Attached Behaviors)
+    /// </summary>
+    public static class WindowBehavior
+    {
+        // ====================================================================
+        // 1. 附加行为: 启用拖动窗口 (EnableDragMove)
+        // ====================================================================
+
+        public static bool GetEnableDragMove(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(EnableDragMoveProperty);
+        }
+
+        public static void SetEnableDragMove(DependencyObject obj, bool value)
+        {
+            obj.SetValue(EnableDragMoveProperty, value);
+        }
+
+        // 使用 'prop' Gist 片段创建附加属性
+        public static readonly DependencyProperty EnableDragMoveProperty =
+            DependencyProperty.RegisterAttached(
+                "EnableDragMove",
+                typeof(bool),
+                typeof(WindowBehavior),
+                new PropertyMetadata(false, OnEnableDragMoveChanged));
+
+        private static void OnEnableDragMoveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is UIElement element)
+            {
+                if ((bool)e.NewValue)
+                {
+                    // 启用: 订阅事件
+                    element.MouseLeftButtonDown += Element_MouseLeftButtonDown;
+                }
+                else
+                {
+                    // 禁用: 取消订阅
+                    element.MouseLeftButtonDown -= Element_MouseLeftButtonDown;
+                }
+            }
+        }
+
+        private static void Element_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // 找到附加此行为的元素所在的窗口
+                Window window = Window.GetWindow(sender as DependencyObject);
+                if (window != null)
+                {
+                    // 执行拖动
+                    window.DragMove();
+                }
+            }
+        }
+
+        // ====================================================================
+        // 2. 附加行为: 触发窗口关闭 (CloseWindowTrigger)
+        // ====================================================================
+
+        public static bool GetCloseWindowTrigger(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(CloseWindowTriggerProperty);
+        }
+
+        public static void SetCloseWindowTrigger(DependencyObject obj, bool value)
+        {
+            obj.SetValue(CloseWindowTriggerProperty, value);
+        }
+
+        public static readonly DependencyProperty CloseWindowTriggerProperty =
+            DependencyProperty.RegisterAttached(
+                "CloseWindowTrigger",
+                typeof(bool),
+                typeof(WindowBehavior),
+                new PropertyMetadata(false, OnCloseWindowTriggerChanged));
+
+        private static void OnCloseWindowTriggerChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // 当 ViewModel 的 'IsCloseRequested' 变为 true 时
+            if (d is Window window && (bool)e.NewValue)
+            {
+                // 关闭窗口
+                window.Close();
+            }
+        }
+    }
+}
---
+++ ServoPress/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace ServoPress.ViewModels
+{
+    /// <summary>
+    /// MainWindow 的 ViewModel，负责处理窗口状态和命令
+    /// </summary>
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        // 1. 绑定到 Window.WindowState
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsMaximized))]
+        private WindowState _windowState = WindowState.Normal;
+
+        // 辅助属性，用于切换最大化/还原按钮的图标
+        public bool IsMaximized => WindowState == WindowState.Maximized;
+
+        // 2. 绑定到一个附加行为，用于触发窗口关闭
+        [ObservableProperty]
+        private bool _isCloseRequested;
+
+        /// <summary>
+        /// 最小化窗口
+        /// </summary>
+        [RelayCommand]
+        private void MinimizeWindow()
+        {
+            // 触发双向绑定，View 将会收到此变更
+            WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// 切换最大化/还原
+        /// </summary>
+        [RelayCommand]
+        private void ToggleMaximizeWindow()
+        {
+            // 触发双向绑定，View 将会收到此变更
+            WindowState = (WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        /// <summary>
+        /// 请求关闭窗口
+        /// </summary>
+        [RelayCommand]
+        private void CloseWindow()
+        {
+            // 触发附加行为
+            IsCloseRequested = true;
+        }
+    }
+}
