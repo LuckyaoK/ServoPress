30607f12c67c07f1333bd73ccfea400e9d7031b1
YaoKai <53244697@qq.com>
2025-11-17 16:56:02 +0800

    更改Ui

--- ServoPress/Views/MainWindow.xaml.cs
+++ ServoPress/Views/MainWindow.xaml.cs
@@ -19,40 +19,6 @@ namespace ServoPress
             InitializeComponent();
         }
 
-        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            // 允许窗口拖动
-            if (e.ButtonState == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
-        }
-
-        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            // 最小化
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
-        {
-            // 最大化/还原
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-            }
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-            // 关闭窗口
-            this.Close();
-        }
-
     }
 
 }
