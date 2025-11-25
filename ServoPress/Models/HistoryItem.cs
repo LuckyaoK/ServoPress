de637de41cae45e343430d2f9f31bdf2f068ed7f
YaoKai <53244697@qq.com>
2025-11-25 11:10:46 +0800

    优化历史界面

--- ServoPress/Models/HistoryItem.cs
+++ ServoPress/Models/HistoryItem.cs
@@ -7,13 +7,7 @@ using System.Threading.Tasks;
 
 namespace ServoPress.Models
 {
-    public class HistoryItem
-    {
-        public string Timestamp { get; set; }
-        public string Program { get; set; }
-        public string Result { get; set; }
-        public double MaxForce { get; set; }
-    }
+
 
     public class PValueConfig
     {
